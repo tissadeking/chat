0c48c531b5dae39cf14769d961ba65d245527df6
tissadeking <34912974+tissadeking@users.noreply.github.com>
2025-02-15 01:29:28 +0100

    Update start_2.py

--- start_2.py
+++ start_2.py
@@ -1,19 +1,14 @@
-#import csv
-#import os
-#from tf_idf_method import main_tf_idf
 from llam3_8 import llm_query_fun_3_8
 from llam3_70 import llm_query_fun_3_70
 from mixtral_groq import llm_query_fun_mixtral
 from gemma2_groq import llm_query_fun_gemma2
-#import time
-#import tracemalloc
 
 #use_case = 'GREEN DEAL'
 print("Welcome to the dataset info retrieval tool!")
 print("Type 'exit' to quit.\n")
 
 def start_fun(user_query, use_case):
-    # open and read the file after the appending:
+    #open and read the model file:
     f = open("model.txt", "r")
     #print(f.read())
     model = f.read()
@@ -25,13 +20,9 @@ def start_fun(user_query, use_case):
     #    print("Goodbye!")
     #    break
     global answer
-    columns = ['use_case', 'time', 'memory', 'query', 'answer', 'length']
+    
     #get answer from the dataset
     for model in models:
-        #answer = ''
-        #data_dict = {}
-        #tracemalloc.start()
-        #start = time.time()
         '''if model == 'tf_idf_method':
             answer = main_tf_idf(user_query)
         if model == 'knl_graph':
@@ -46,26 +37,4 @@ def start_fun(user_query, use_case):
         if model == 'llama-3.3-70b':
             answer = llm_query_fun_3_70(user_query)
 
-        '''end = time.time()
-        current_usage, peak_usage = tracemalloc.get_traced_memory()
-        tracemalloc.stop()
-        file_name = model + '.csv'
-        data_dict['use_case'] = use_case
-        data_dict['time'] = end - start
-        data_dict['memory'] = current_usage
-        data_dict['query'] = user_query
-        data_dict['answer'] = answer
-        answer_main = answer
-        answer = answer.replace(' ', '')
-        data_dict['length'] = len(answer)
-        print('data dict: ', data_dict)
-        if os.path.exists(file_name) == True:
-            with open(file_name, 'a') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=columns)
-                writer.writerow(data_dict)
-        elif os.path.exists(file_name) == False:
-            with open(file_name, 'a') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=columns)
-                writer.writeheader()
-                writer.writerow(data_dict)'''
     return answer
